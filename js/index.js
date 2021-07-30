a9033e51907f06b32c9878f014f4bd9691aeb5d8
v2r2please <76893867+v2r2please@users.noreply.github.com>
2021-07-30 12:57:20 -0700

    Layout and some styling done
    To Do:
    Write copy
    Include a GIF of prototype
    Link to prototype
    Modal for contact form if we are feeling skippy.

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$(function () {
+    $('a[href*=\\#]:not([href=\\#])').on('click', function () {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.substr(1) + ']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    });
+});
+$(window).scroll(function () {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function () {
+    $("#back2Top").click(function (event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
